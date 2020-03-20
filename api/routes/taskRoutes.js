53cb39b246ae54407e6b3ad8f9ec2be4168ab9c8
TAISHI SENOH <mokut2017@gmail.com>
2020-03-20 18:43:40 +0900

    route settings
    ルーティングとリクエスト毎の応答の設定

---
+++ api/routes/taskRoutes.js
@@ -0,0 +1,12 @@
+module.exports = function(app) {
+  var taskList = require('../controllers/taskController');
+
+  app.route('/tasks')
+    .get(taskList.all_tasks)
+    .post(takskList.cereate_task);
+
+  app.route('/tasks/:taskId')
+    .get(taskList.load_task)
+    .put(taskList.update_task)
+    .delete(taskList.delete_task);
+};
